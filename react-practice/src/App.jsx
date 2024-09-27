66992713632b5978b21f2999816e73baea26c4fb
hitmananas <anasfarooq2006@gmail.com>
2024-09-27 20:34:37 +0500

    Installed tailwind css

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,10 +1,11 @@
-export const App = () => {
+const App = () => {
 
-return ( 
-<div>
-  <div><img src="practice.jpg" alt="practice" width="80%" height="80%" /></div>
-   <h2>Name: Queen of Tears</h2> 
-<h3>Rating:8.2</h3>
-<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate possimus accusantium impedit architecto, ab eum perspiciatis debitis neque quae fugiat dignissimos magnam omnis molestias mollitia, porro aspernatur! Architecto, laudantium voluptate debitis nesciunt error dolorum esse voluptatibus. Sunt animi debitis odio nobis ullam! Delectus corporis magni repudiandae nisi asperiores nam dolorem!</p>
-</div>)
+  return(
+
+    <div> <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1></div>
+  )
 }
+
+export default App
